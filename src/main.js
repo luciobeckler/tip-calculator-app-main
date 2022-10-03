233f8549ac87eb3a58a86de8d1669a4de5be2da3
Lucio <luciobecklerp@gmail.com>
2022-10-03 13:39:42 -0300

    Botão reset OK

--- src/main.js
+++ src/main.js
@@ -10,16 +10,13 @@ const totalDisplay = document.getElementById('totalPerson')
 const resetButton = document.querySelector('.resetButton')
 
 let bill = 0.0
-let percentage = 0
+let percentage = 0.0
 let people = 1
 calculaEExibe()
 
 
-/* while (isNaN(bill * percentage * people)){
-} */
 totalBill.addEventListener('input', function () {
       bill = parseFloat(totalBill.value)
-      console.log(typeof (bill), bill)
       calculaEExibe()
 })
 
@@ -30,31 +27,44 @@ for (let i = 0; i < tipButton.length; i++) {
             }
             tipButton[i].classList.add('selectedButton');
             percentage = parseFloat(tipButton[i].value)
-            console.log(typeof (percentage), percentage)
             calculaEExibe()
       })
 }
 
 peopleInput.addEventListener('input', function () {
       people = parseInt(peopleInput.value)
-      console.log(typeof (people), people)
+      if (people == 0) {
+            cantBeZeroText.style.display = 'flex'
+            peopleInput.style.borderColor = 'rgb(250, 116, 6)'
+      }
+      else {
+            cantBeZeroText.style.display = 'none'
+            peopleInput.style.borderColor = 'hsl(172, 67%, 45%)'
+      }
       calculaEExibe()
 
 })
 
 function calculaEExibe() {
       let tipPerson = (parseFloat(bill * percentage / (100 * people))).toFixed(2)
-      let tipTotal = (bill * percentage / 100).toFixed(2)
+      let tipTotal = (parseFloat(bill * percentage / 100)).toFixed(2)
 
-      if (isNaN(tipPerson)) tipDisplay.innerHTML = '0.0'
+      if (isNaN(tipPerson)) tipDisplay.innerHTML = '0.00'
       else {
             tipDisplay.innerHTML = tipPerson
       }
 
-      if (isNaN(tipTotal)) totalDisplay.innerHTML = '0.0'
+      if (isNaN(tipTotal)) totalDisplay.innerHTML = '0.00'
       else {
             totalDisplay.innerHTML = tipTotal
       }
-
-
 }
+
+resetButton.addEventListener('click', function () {
+      for (let j = 0; j < tipButton.length; j++) {         /*Sempre que ocorre um novo clique este for realoca a classe 'selectedCircle' */
+            tipButton[j].classList.remove('selectedButton')
+      }
+      tipDisplay.innerHTML = '0.00'
+      totalDisplay.innerHTML = '0.00'
+
+})
