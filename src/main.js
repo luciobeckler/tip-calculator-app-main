943be13828395a507829cb7385c9cd320a8d81eb
Lucio <luciobecklerp@gmail.com>
2022-10-04 14:05:36 -0300

    Projeto faltando revisão

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /* Variáveis  */
 const totalBill = document.getElementById('totalBill')
 const tipButton = document.querySelectorAll('.tipButton')
-const tipInput = document.querySelector('.tipInput')
+const customInput = document.querySelector('.customInput')
 const peopleInput = document.querySelector('.peopleInput')
 const cantBeZeroText = document.querySelector('.cantBeZeroText')
 
@@ -12,24 +12,40 @@ const resetButton = document.querySelector('.resetButton')
 let bill = 0.0
 let percentage = 0.0
 let people = 1
-calculaEExibe()
+computAndDisplay()
+
+
+
+
 
 
 totalBill.addEventListener('input', function () {
       bill = parseFloat(totalBill.value)
-      calculaEExibe()
+      computAndDisplay()
 })
 
+
+/* ARRUMAR O BOTÃO QUE DEVERIA TIRAR A CLASSE SELECTEDBUTTON EM CASO DE CLIQUE DUPLO */
 for (let i = 0; i < tipButton.length; i++) {
       tipButton[i].addEventListener('click', function () {
             for (let j = 0; j < tipButton.length; j++) {         /*Sempre que ocorre um novo clique este for realoca a classe 'selectedCircle' */
-                  tipButton[j].classList.remove('selectedButton')
+                  if (tipButton[j].classList.contains('selectedButton')) tipButton[j].classList.remove('selectedButton')
             }
             tipButton[i].classList.add('selectedButton');
             percentage = parseFloat(tipButton[i].value)
-            calculaEExibe()
+            computAndDisplay()
       })
 }
+
+customInput.addEventListener('click', function () {
+      for (let j = 0; j < tipButton.length; j++) {         /*Sempre que ocorre um novo clique este for realoca a classe 'selectedCircle' */
+            if (tipButton[j].classList.contains('selectedButton')) tipButton[j].classList.remove('selectedButton')
+      }
+      customInput.addEventListener('input', function () {
+            percentage = parseFloat(customInput.value)
+            computAndDisplay()
+      })
+})
 
 peopleInput.addEventListener('input', function () {
       people = parseInt(peopleInput.value)
@@ -41,11 +57,11 @@ peopleInput.addEventListener('input', function () {
             cantBeZeroText.style.display = 'none'
             peopleInput.style.borderColor = 'hsl(172, 67%, 45%)'
       }
-      calculaEExibe()
+      computAndDisplay()
 
 })
 
-function calculaEExibe() {
+function computAndDisplay() {
       let tipPerson = (parseFloat(bill * percentage / (100 * people))).toFixed(2)
       let tipTotal = (parseFloat(bill * percentage / 100)).toFixed(2)
 
